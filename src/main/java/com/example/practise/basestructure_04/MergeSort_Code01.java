183ef170e1dc50008378803584d754efb7f6d5cc
HaoKangHao <1545731263@qq.com>
2021-04-12 20:24:51 +0800

    归并排序递归/非递归/最小和问题

---
+++ src/main/java/com/example/practise/basestructure_04/MergeSort_Code01.java
@@ -0,0 +1,160 @@
+package com.example.practise.basestructure_04;
+
+/**
+ * @author:haokanghao
+ * @date: 2021/4/12 19:43
+ * @desc: 归并/非递归 排序，使用递归
+ */
+public class MergeSort_Code01 {
+
+    // 递归方法实现
+    public static void mergeSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process(arr, 0, arr.length - 1);
+    }
+
+    /**
+     *
+     */
+    private static void process(int[] arr,int L, int R) {
+        if(L == R){
+            return;
+        }
+        int mid = L+(R-L)>>1;
+        process(arr,L,mid);
+        process(arr,mid+1,R);
+        merge(arr,L,mid,R);
+    }
+
+    /**
+     * 左数组  右数组
+     * 生成一个新的数组，然后左和右进行对比排好序 ，只遍历N次
+     *
+     */
+    private static void merge(int[] arr, int L, int M, int R) {
+        int[] help = new int[(R-L)+1];
+        int i = 0;
+        int p1 = L;
+        int p2 = M + 1;
+        while (p1 <= M && p2 <= R) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1<=M){
+            help[i++] = arr[p1++];
+        }
+        while (p2<=R){
+            help[i++] = arr[p2++];
+        }
+        for(i =0 ;i<arr.length;i++){
+            arr[L+i] = help[i];
+        }
+    }
+
+    /**
+     * 非递归的方法采用部长
+     * @param arr
+     */
+
+    public static void mergeSort2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int N = arr.length;
+        // 步长
+        int mergeSize = 1;
+        while (mergeSize < N) { // log N
+            // 当前左组的，第一个位置
+            int L = 0;
+            while (L < N) {
+                if (mergeSize >= N - L) {
+                    break;
+                }
+                int M = L + mergeSize - 1;
+                int R = M + Math.min(mergeSize, N - M - 1);
+                merge(arr, L, M, R);
+                L = R + 1;
+            }
+            // 防止溢出
+            if (mergeSize > N / 2) {
+                break;
+            }
+            mergeSize <<= 1;
+        }
+    }
+
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            mergeSort1(arr1);
+            mergeSort2(arr2);
+            if (!isEqual(arr1, arr2)) {
+                System.out.println("出错了！");
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+
+
+}
