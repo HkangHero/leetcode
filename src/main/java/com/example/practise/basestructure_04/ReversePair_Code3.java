8bdfe4c4e0016845e185d9edf48d319dfb737740
HaoKangHao <1545731263@qq.com>
2021-04-13 10:06:35 +0800

    归并 逆序对问题

--- src/main/java/com/example/practise/basestructure_04/ReversePair_Code3.java
+++ src/main/java/com/example/practise/basestructure_04/ReversePair_Code3.java
@@ -2,70 +2,81 @@ package com.example.practise.basestructure_04;
 
 /**
  * @author:haokanghao
- * @date: 2021/4/12 20:23
- * @desc: 最小和问题
+ * @date: 2021/4/13 08:40
+ * @desc: 求一个数组中逆序对的个数
+ * arr[9,10,6,12,11,8]
+ * 逆序对： 9,6  9,8  10,6  12,11 12,8
+ *
+ * 求解：假设：[9,6,10] [8,11,12]  都已经排好序
+ *  l:左边数组的指针 (M) 左边是倒退
+ *  r：右边数组的指针（R）右边也是倒退
+ *
+ *  如果arr[l]>arr[r] 符合 那么对于arr[l]就有 r-M 对
+ *
+ *  如果能排好序并且单调增或者减可以解决 可以用归并去解决
+ *
+ *  逆序对： 左大  右小
+ *
+ *
  */
-public class MergeSort_Code02 {
-    public static int smallSum(int[] arr) {
+public class ReversePair_Code3 {
+    public static int reverPairNumber(int[] arr) {
         if (arr == null || arr.length < 2) {
             return 0;
         }
         return process(arr, 0, arr.length - 1);
     }
 
-    // arr[L..R]既要排好序，也要求小和返回
-    // 所有merge时，产生的小和，累加
-    // 左 排序   merge
-    // 右 排序  merge
-    // merge
-    public static int process(int[] arr, int l, int r) {
-        if (l == r) {
+
+    public static int process(int[] arr, int L, int R) {
+        if(L == R){
             return 0;
         }
-        // l < r
-        int mid = l + ((r - l) >> 1);
-        return
-                process(arr, l, mid)
-                        +
-                        process(arr, mid + 1, r)
-                        +
-                        merge(arr, l, mid, r);
+        int M = L+ ((R-L)>>1); //todo: 记住 运算符优先级
+        return process(arr,L,M)+process(arr,M+1,R)+merge(arr,L,M,R);
     }
 
-    public static int merge(int[] arr, int L, int m, int r) {
-        int[] help = new int[r - L + 1];
-        int i = 0;
-        int p1 = L;
-        int p2 = m + 1;
-        int res = 0;
-        while (p1 <= m && p2 <= r) {
-            res += arr[p1] < arr[p2] ? (r - p2 + 1) * arr[p1] : 0;
-            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+    // arr[L..R]既要排好序，也要求逆序对数量返回
+    // 所有merge时，产生的逆序对数量，累加，返回
+    // 左 排序 merge并产生逆序对数量
+    // 右 排序 merge并产生逆序对数量
+
+    public static int merge(int[] arr, int L, int M, int R) {
+        int[] help = new int[R - L + 1];
+        int i = help.length-1;
+        int p1 = M;
+        int p2 = R;
+        int count = 0;
+
+        while (p1>=L&&p2>M){
+          count += arr[p1]>arr[p2]?(p2-M):0;
+          help[i--] = arr[p1]>arr[p2]?arr[p1--]:arr[p2--];
         }
-        while (p1 <= m) {
-            help[i++] = arr[p1++];
+        while (p1>=L){
+            help[i--] = arr[p1--];
         }
-        while (p2 <= r) {
-            help[i++] = arr[p2++];
+        while (p2>M){
+            help[i--] = arr[p2--];
         }
-        for (i = 0; i < help.length; i++) {
-            arr[L + i] = help[i];
+        for(i =0 ;i<help.length;i++){
+            arr[L+i] = help[i];
         }
-        return res;
+
+        return  count;
     }
+
 
     // for test
     public static int comparator(int[] arr) {
-        if (arr == null || arr.length < 2) {
-            return 0;
-        }
-        int res = 0;
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = 0; j < i; j++) {
-                res += arr[j] < arr[i] ? arr[j] : 0;
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    ans++;
+                }
             }
         }
-        return res;
+        return ans;
     }
 
     // for test
@@ -124,17 +135,17 @@ public class MergeSort_Code02 {
         int testTime = 500000;
         int maxSize = 100;
         int maxValue = 100;
-        boolean succeed = true;
+        System.out.println("测试开始");
         for (int i = 0; i < testTime; i++) {
             int[] arr1 = generateRandomArray(maxSize, maxValue);
             int[] arr2 = copyArray(arr1);
-            if (smallSum(arr1) != comparator(arr2)) {
-                succeed = false;
+            if (reverPairNumber(arr1) != comparator(arr2)) {
+                System.out.println("Oops!");
                 printArray(arr1);
                 printArray(arr2);
                 break;
             }
         }
-        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+        System.out.println("测试结束");
     }
 }
